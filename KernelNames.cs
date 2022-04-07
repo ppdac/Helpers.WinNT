ba0ecd02d069f0738ffc8a0f763d719f947cd173
Louis Waweru <louis@waweru.me>
2022-04-07 10:34:20 -0400

    Outdated should return -1 and Unknown is probably new

--- KernelNames.cs
+++ KernelNames.cs
@@ -6,22 +6,21 @@ namespace Helpers.WinNT
     {
         public enum KernelName
         {
-            WinUnkown = -1,
-            Vista,
+            WinOutdated = -1,
+            Vista = 0,
             Windows7 = 7,
             Windows8 = 8,
             Windows10 = 10,
             Windows11 = 11,
             Windows = Windows10 | Windows11,
-            //WinUnkown, #Roslyn increments up one making this 12. Final position would be 6.
-            WinOutdated = 5,
+            WinUnkown = Vista | Windows7| Windows8 | Windows
         };
         
         public static KernelName GetWindowsNTVersion()
         {
             string NTx = Environment.OSVersion.ToString();
 
-            if (NTx.Contains("10.0"))
+            if (NTx.Contains("10.0."))
                 return KernelName.Windows;
             else if (NTx.Contains("6.3.") || NTx.Contains("6.2."))
                 return KernelName.Windows8;
@@ -29,7 +28,7 @@ namespace Helpers.WinNT
                 return KernelName.Windows7;
             else if (NTx.Contains("6.0."))
                 return KernelName.Vista;
-            else if (NTx.Contains("5.2.") || NTx.Contains("5.1.") || NTx.Contains("5.1."))
+            else if (NTx.Contains("5.2.") || NTx.Contains("5.1.") || NTx.Contains("5.0."))
                 return KernelName.WinOutdated;
 
             return KernelName.WinUnkown;
