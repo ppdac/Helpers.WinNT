7904bbf0bbc9f0a072a2ece7e4d8b94a553245fd
Louis Waweru <louis@waweru.me>
2022-04-07 10:48:21 -0400

    Treat EOL & Unknown as unsupported
    Actually this will run cross platform, but it's not intended to.

--- KernelNames.cs
+++ KernelNames.cs
@@ -6,21 +6,24 @@ namespace Helpers.WinNT
     {
         public enum KernelName
         {
+            WinUnknown = -1
             WinOutdated = -1,
+            Unsupported = WinUnknown | WinOutdated
             Vista = 0,
             Windows7 = 7,
             Windows8 = 8,
             Windows10 = 10,
             Windows11 = 11,
-            Windows = Windows10 | Windows11,
-            WinUnkown = Vista | Windows7| Windows8 | Windows
+            Windows = Windows10 | Windows11
         };
         
         public static KernelName GetWindowsNTVersion()
         {
             string NTx = Environment.OSVersion.ToString();
 
-            if (NTx.Contains("10.0."))
+            if (NTx.Contains("Unix"))
+                return KernelName.Unsupported;
+            else if (NTx.Contains("10.0."))
                 return KernelName.Windows;
             else if (NTx.Contains("6.3.") || NTx.Contains("6.2."))
                 return KernelName.Windows8;
@@ -31,7 +34,7 @@ namespace Helpers.WinNT
             else if (NTx.Contains("5.2.") || NTx.Contains("5.1.") || NTx.Contains("5.0."))
                 return KernelName.WinOutdated;
 
-            return KernelName.WinUnkown;
+            return KernelName.WinUnknown;
         }
     }
 }
